8a4bf7b4f358f30a1d9953455add38a507aed829
Marcio-40-mrn <marciorpnascimento@hotmail.com>
2026-06-30 20:45:56 -0300

    fix

--- test/pageobjects/HomePage.ts
+++ test/pageobjects/HomePage.ts
@@ -18,19 +18,19 @@ export class HomePage extends BasePage {
         }
     }
 
-    // async abrirPerfil() {
-    //     const element = await $("accessibility id:Menu");
-    //     await this.waitForElement(element);
-    //     await element.click();
-    //     await driver.pause(timewhait);
-    // }
-
     async abrirPerfil() {
-        const element = await $("accessibility id:Perfil");
+        const element = await $("accessibility id:Menu");
         await this.waitForElement(element);
         await element.click();
         await driver.pause(timewhait);
     }
+
+    // async abrirPerfil() {
+    //     const element = await $("accessibility id:Perfil");
+    //     await this.waitForElement(element);
+    //     await element.click();
+    //     await driver.pause(timewhait);
+    // }
 
 
     async abrirCategorias() {
